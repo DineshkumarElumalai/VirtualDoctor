802d540d5b4e144b34c039346a13ec868da75b7c
Dineshkumar Elumalai <dineshkumare@FDLMC1833.local>
2018-07-11 15:33:31 +0530

    first commit

---
+++ src/main/java/com/freshworks/virtualdoctor/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.freshworks.virtualdoctor.model;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_DOCTOR,
+    ROLE_PATIENT
+}
---
+++ src/main/java/com/freshworks/virtualdoctor/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.freshworks.virtualdoctor.repository;
+
+
+import com.freshworks.virtualdoctor.model.Role;
+import com.freshworks.virtualdoctor.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
